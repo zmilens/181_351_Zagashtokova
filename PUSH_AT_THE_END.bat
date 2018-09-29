59ac0195e6a0705dc9cb75874dd28852183b5416
zmilens <milenabariat@mail.ru>
2018-09-29 12:36:41 +0300

    29.09.2018 12:36:40,90
    Signed-off-by: zmilens <milenabariat@mail.ru>

---
+++ PUSH_AT_THE_END.bat
@@ -0,0 +1,10 @@
+﻿git add *.cpp
+git add *.h 
+git add *.sln 
+git add *.vcxproj 
+git add *.vcxproj.filters 
+git add *.user
+git add *.bat
+git commit -sm "%DATE% %TIME%"
+git push zmilens master
+pause
